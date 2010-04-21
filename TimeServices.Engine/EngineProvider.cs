1efca945a2121cd7f45c05387503ea8ef66541e6
Moreys <Moreys@localhost>
2010-04-20 20:12:44 -0500

    Reorder

--- TimeServices.Engine/EngineProvider.cs
+++ TimeServices.Engine/EngineProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using Microsoft.Win32;
 namespace TimeServices.Engine
 {
     public static class EngineProvider
@@ -8,17 +9,17 @@ namespace TimeServices.Engine
 
         static EngineProvider()
         {
-            Assembly assembly; 
-            int caps = 1;
+            Assembly assembly;
+            int caps = GetTightestProvider();
             switch (caps)
             {
                 case 1:
-                    assembly = Assembly.LoadFile(@"C:\_ROAMING\_APPLICATION\GOOGLE_\TIMESERVICES\x64\Debug\TimeServices.Engine.AtiStreams.dll");
-                    s_engineProviderType = assembly.GetType("TimeServices.Engine.CalEngineProvider", true);
-                    break;
-                case 2:
                     assembly = Assembly.LoadFile(@"C:\_ROAMING\_APPLICATION\GOOGLE_\TIMESERVICES\x64\Debug\TimeServices.Engine.Cuda.dll");
                     s_engineProviderType = assembly.GetType("TimeServices.Engine.CudaEngineProvider", true);
+                    break;
+                case 2:
+                    assembly = Assembly.LoadFile(@"C:\_ROAMING\_APPLICATION\GOOGLE_\TIMESERVICES\x64\Debug\TimeServices.Engine.AtiStreams.dll");
+                    s_engineProviderType = assembly.GetType("TimeServices.Engine.CalEngineProvider", true);
                     break;
                 default:
                     assembly = Assembly.LoadFile(@"C:\_ROAMING\_APPLICATION\GOOGLE_\TIMESERVICES\x64\Debug\TimeServices.Engine.Cpu.dll");
@@ -30,6 +31,22 @@ namespace TimeServices.Engine
         public static IEngineProvider CreateProvider()
         {
             return (Activator.CreateInstance(s_engineProviderType) as IEngineProvider);
+        }
+
+        public static int GetTightestProvider()
+        {
+            return (GetHasCuda() ? 1 : (GetHasCal() ? 2 : 0));
+        }
+
+        public static bool GetHasCuda()
+        {
+            string cudaPath = (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\NVIDIA Corporation\Installed Products\NVIDIA CUDA", "InstallDir", null) as string);
+            return (!string.IsNullOrEmpty(cudaPath));
+        }
+
+        public static bool GetHasCal()
+        {
+            return false;
         }
     }
 }
